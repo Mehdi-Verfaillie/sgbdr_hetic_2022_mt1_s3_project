aefccc7b66fe8336ce22e156b6febf55b052e71b
Mehdi <mehdi@ublo.immo>
2022-01-28 12:34:12 +0100

    [Add]: Poster component base

---
+++ src/components/Poster.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+interface Props {
+    title: string;
+    price: string;
+    rank: string;
+    genre: string;
+    rantal: string;
+}
+
+export const Poster = (props: Props): JSX.Element => {    
+    return (
+        <Container>
+            Titre: {props.title}
+            Prix: {props.price}
+            Classement: {props.rank}
+            Genre: {props.genre}
+            Loué {props.rantal} fois
+        </Container>
+    )
+}
+
+const Container = styled("div")`
+    background: gray;
+    width: 200px;
+    height: 350px;
+`

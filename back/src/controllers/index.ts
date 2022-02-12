b7d6549742b00f56695baf6079c1cdc36f576d3f
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-02-12 18:17:51 +0100

    [Add]: (WIP): movies controller

--- back/src/controllers/index.ts
+++ back/src/controllers/index.ts
@@ -1,3 +1,4 @@
+import { Connect, Query } from "@/config/database.module";
 import { Request, Response } from "express";
 
 /**
@@ -13,5 +14,31 @@ export const index = async (req: Request, res: Response): Promise<void> => {
  * Movie list.
  */
 export const movies = async (req: Request, res: Response): Promise<void> => {
-    res.json({ path: "movies" });
+    const query = "SELECT DISTINCT category.name AS category, film.title, film.rating, rental_rate, COUNT(rental.rental_id) AS rental FROM category  JOIN film_category ON category.category_id = film_category.category_id  JOIN film ON film_category.film_id = film.film_id  JOIN inventory ON film.film_id = inventory.film_id JOIN rental ON inventory.inventory_id = rental.inventory_id JOIN payment ON rental.rental_id = payment.rental_id GROUP BY film.film_id";
+
+    Connect()
+        .then((connection) => {
+            Query(connection, query)
+                .then((results) => {
+                    return res.status(200).json({
+                        results,
+                    });
+                })
+                .catch((error) => {
+                    return res.status(200).json({
+                        message: error.message,
+                        error,
+                    });
+                })
+                .finally(() => {
+                
+                    connection.end();
+                });
+        })
+        .catch((error) => {
+            return res.status(200).json({
+                message: error.message,
+                error,
+            });
+        });
 };

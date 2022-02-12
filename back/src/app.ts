570b41678aa1fca010160a9647b2afb1fc4e4039
Mehdi <mehdi@ublo.immo>
2022-02-12 20:35:09 +0100

    [Add]: cors middleware

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import * as path from "path";
+import cors from "cors";
 
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
@@ -9,6 +10,7 @@ import { api, index } from "./routes/index";
 // Create Express server
 export const app = express();
 
+app.use(cors());
 // Express configuration
 app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "../views"));
@@ -17,6 +19,7 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
+
 app.use("/", index);
 app.use("/api", api);
 

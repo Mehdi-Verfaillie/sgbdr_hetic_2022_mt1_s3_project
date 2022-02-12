e2a7b037070ff041ec2e5b9b9f68d688bcbdd207
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-02-12 17:45:08 +0100

    [Add]: database connection configuration

---
+++ back/src/config/database.module.ts
@@ -0,0 +1,38 @@
+import mysql from 'mysql';
+import config from './config';
+
+const params = {
+    host: config.mysql.host,
+    port: config.mysql.port,
+    database: config.mysql.database,
+    user: config.mysql.user,
+    password: config.mysql.password,
+};
+
+const Connect = async (): Promise<mysql.Connection> =>
+    new Promise<mysql.Connection>((resolve, reject) => {
+        const connection = mysql.createConnection(params);
+
+        connection.connect((error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve(connection);
+        });
+    });
+
+const Query = async (connection: mysql.Connection, query: string): Promise<unknown> =>
+    new Promise((resolve, reject) => {
+        connection.query(query, connection, (error, result) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve(result);
+        });
+    });
+
+export { Connect, Query };

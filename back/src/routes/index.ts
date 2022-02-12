bbff92d656beed70aca5c74edf676de8712e5a72
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-02-12 17:32:58 +0100

    [Add]: /movies route

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -2,5 +2,7 @@ import { Router } from "express";
 import * as controller from "../controllers/index";
 
 export const index = Router();
+export const api = Router();
 
 index.get("/", controller.index);
+api.get("/movies", controller.movies);

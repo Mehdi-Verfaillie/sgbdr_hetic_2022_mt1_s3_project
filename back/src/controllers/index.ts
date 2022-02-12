a23824cde82c4d5e785f841d428c94212dfbef48
Mehdi <mehdi@ublo.immo>
2022-02-12 23:12:09 +0100

    [Fix]: replace .then by async await

--- back/src/controllers/index.ts
+++ back/src/controllers/index.ts
@@ -1,5 +1,6 @@
-import { Connect, Query } from "@/config/database.module";
+import { Connect, Query } from "../config/database.module";
 import { Request, Response } from "express";
+import { getMovies, totalMoviesQuery } from "./types/queries";
 
 /**
  * GET /
@@ -14,31 +15,25 @@ export const index = async (req: Request, res: Response): Promise<void> => {
  * Movie list.
  */
 export const movies = async (req: Request, res: Response): Promise<void> => {
-    const query = "SELECT DISTINCT category.name AS category, film.title, film.rating, rental_rate, COUNT(rental.rental_id) AS rental FROM category  JOIN film_category ON category.category_id = film_category.category_id  JOIN film ON film_category.film_id = film.film_id  JOIN inventory ON film.film_id = inventory.film_id JOIN rental ON inventory.inventory_id = rental.inventory_id JOIN payment ON rental.rental_id = payment.rental_id GROUP BY film.film_id";
+    const { page, limit }: { page: number, limit: number } = req.query;
+    
+    try {
+        const connection = await Connect();
 
-    Connect()
-        .then((connection) => {
-            Query(connection, query)
-                .then((results) => {
-                    return res.status(200).json({
-                        results,
-                    });
-                })
-                .catch((error) => {
-                    return res.status(200).json({
-                        message: error.message,
-                        error,
-                    });
-                })
-                .finally(() => {
-                
-                    connection.end();
-                });
-        })
-        .catch((error) => {
-            return res.status(200).json({
-                message: error.message,
-                error,
-            });
+        const movies = await Query(connection, getMovies(limit, page));
+        const total = await Query(connection, totalMoviesQuery);
+
+        res.status(200).json({
+            results: movies,
+            total: total[0].total / limit,
         });
+
+        connection.end();
+
+    } catch (error) {
+        res.status(200).json({
+            message: error.message,
+            error,
+        });
+    }
 };

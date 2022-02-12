8071c3dd4b2bdf6e367e54737a7b3014bcbdfd8a
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-02-12 16:27:39 +0100

    [Add]: error/loading text style

--- front/src/components/StatefulList.tsx
+++ front/src/components/StatefulList.tsx
@@ -2,6 +2,7 @@ import { Movie } from "../api/types";
 import React from "react";
 import { List } from "./List";
 import { Pagination } from "./Pagination";
+import styled from "styled-components";
 
 interface Props {
   isLoading: boolean;
@@ -11,9 +12,9 @@ interface Props {
 }
 
 export const StatefulList = (props: Props): JSX.Element => {
-  if (props.isLoading) return <div>loading...</div>;
-  if (props.isError) return<div>error...</div>;
-  if (props.isEmpty) return <div>empty list...</div>;
+  if (props.isLoading) return <Text>loading...</Text>;
+  if (props.isError) return<Text>error...</Text>;
+  if (props.isEmpty) return <Text>empty list...</Text>;
 
   return (
     <div>
@@ -22,3 +23,13 @@ export const StatefulList = (props: Props): JSX.Element => {
     </div>
   );
 };
+
+const Text = styled("p")`
+     font-size: 1.25rem;
+    font-weight: 500;
+    letter-spacing: 0.5px;
+    line-height: 1.1875;
+    font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    color: rgb(255, 255, 255);
+    margin: 0.5rem 0;
+`

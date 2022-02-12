a9efe789e02a66cdf13cd2419ecc28fefd82b72d
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-02-12 17:32:46 +0100

    [Add]: base Movie controller

--- back/src/controllers/index.ts
+++ back/src/controllers/index.ts
@@ -7,3 +7,11 @@ import { Request, Response } from "express";
 export const index = async (req: Request, res: Response): Promise<void> => {
     res.render("index", { title: "Express" });
 };
+
+/**
+ * GET /
+ * Movie list.
+ */
+export const movies = async (req: Request, res: Response): Promise<void> => {
+    res.json({ path: "movies" });
+};

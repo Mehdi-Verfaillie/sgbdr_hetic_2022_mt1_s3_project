fac6f821e97ad339cba58cfa26d07387c80f996a
Mehdi <mehdi@ublo.immo>
2022-02-13 11:29:08 +0100

    [Fix]: transform function to useCallback functions

--- front/src/context/pagination.provider.tsx
+++ front/src/context/pagination.provider.tsx
@@ -19,18 +19,14 @@ const useCreatePaginationStore = (defaultValues?: DefaultValues) => {
   const [pageSize] = useState(defaultValues?.pageSize ?? DEFAULT_PAGE_SIZE);
   const [total, setTotal] = useState(defaultValues?.total ?? 0);
   const [orderType, setOrderType] = useState(defaultValues?.orderType ?? "ASC");
-  const [column, setColumn] = useState("film");
+  const [column, setColumn] = useState(defaultValues?.column ?? "film");
 
   const pageLimit = useMemo(() => getLimit(total, pageSize), [total, pageSize]);
-  const _setColumn = (column: "film" | "category" | "rental") => setColumn(column);
-  const setOrder = (orderType: "ASC" | "DESC") => setOrderType(orderType);
- 
-  const goToPage = useCallback(
-    (page: number) => {
-      setCurrentPage(clamp(page, 1, pageLimit));
-    },
-    [pageLimit]
-  );
+  
+  const _setColumn = useCallback((column: "film" | "category" | "rental") => setColumn(column),[]);
+  const setOrder = useCallback((orderType: "ASC" | "DESC") => setOrderType(orderType), []);
+
+  const goToPage = useCallback((page: number) => setCurrentPage(clamp(page, 1, pageLimit)), [pageLimit]);
 
   const nextPage = useCallback(() => goToPage(currentPage + 1), [currentPage, goToPage]);
   const previousPage = useCallback(() => goToPage(currentPage - 1), [currentPage, goToPage]);
